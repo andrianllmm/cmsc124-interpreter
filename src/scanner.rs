c3a342e3a6642a255dc7d011178e7bb966410bdf
jtmedalla <243247066+jtmedalla@users.noreply.github.com>
2026-09-21 22:06:45 +0800

    fix: throw error for adjacent characters and underscore

--- src/scanner.rs
+++ src/scanner.rs
@@ -250,20 +250,13 @@ impl Scanner {
         let mut s: String = start.to_string();
         let mut is_float: bool = false;
 
-        while !self.is_at_end()
-            && self.peek() != '\n'
-            && !{ [' ', '\t', '\r'].contains(&self.peek()) }
-        {
-            let c: char = self.peek();
+        while !self.is_at_end() && self.peek().is_ascii_digit() || self.peek() == '.' {
+            let c: char = self.advance();
 
             // check if number is valid digit or dot
-            if !c.is_ascii_digit() && (c != '.' || is_float) {
-                if c != '_' || !c.is_ascii_alphabetic(){
-                    break;
-                } else {
-                    self.error(ScanErrorKind::InvalidNumber);
-                    return;
-                }
+            if !c.is_ascii_digit() && (c != '.' || is_float) && c == '_' || c.is_ascii_alphabetic()
+            {
+                break;
             }
 
             // check for float type
@@ -273,8 +266,11 @@ impl Scanner {
 
             // add number to literal
             s.push(c);
+        }
 
-            self.current += 1
+        if self.peek() == '_' || self.peek().is_ascii_alphabetic() {
+            self.error(ScanErrorKind::UnexpectedChar(self.peek()));
+            return;
         }
 
         if s.starts_with('.') || s.ends_with('.') {
